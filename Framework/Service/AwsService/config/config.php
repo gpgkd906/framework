0f818107e9bf90ccf186d69345f08799135ed535
gpgkd906 <gpgkd906@gmail.com>
2015-11-18 23:33:13 +0900

    Update config.php

--- Framework/Service/AwsService/config/config.php
+++ Framework/Service/AwsService/config/config.php
@@ -2,47 +2,5 @@
 
 return [
     "pool" => [
-        [
-            "api_key" => 'AKIAJDT6JVPM722JK2TQ',
-            "secret_key" => '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',
-            "associate_tag" => 'gpgkd90601-2',
-            "filter" => "lookup, jp, cn, ca",
-        ],
-        [
-            "api_key" => 'AKIAIRJFATC4EGHPB6RA',
-            "secret_key" => '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',
-            "associate_tag" => 'chen01-1',
-            "filter" => "lookup, us, es",
-        ],
-        [
-            "api_key" => 'AKIAJS6RRM4CJEIDF7OQ',
-            "secret_key" => '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',
-            "associate_tag" => 'chen01-2',
-            "filter" => "lookup, de, it",
-        ],
-        [
-            "api_key" => 'AKIAIAIDR433P7SUX4UQ',
-            "secret_key" => '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',
-            "associate_tag" => 'chen@penseur.co.jp',
-            "filter" => "lookup, fr, uk",
-        ],
-        [
-            "api_key" => 'AKIAJL2PS6NIFYCUTUMQ',
-            "secret_key" => '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',
-            "associate_tag" => 'chen@penseur.co.jp2',
-            "filter" => "lookup, us, jp"
-        ],
-        [
-            "api_key" => 'AKIAJCY7AUFRIRFQF6EA',
-            "secret_key" => '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',
-            "associate_tag" => 'tomomasa-1',
-            "filter" => "browsenode"
-        ],
-        [
-            "api_key" => 'AKIAIHTTJA2U3YEENC7A',
-            "secret_key" => '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',
-            "associate_tag" => 'tomomasa-2',
-            "filter" => "browsenode"
-        ],        
     ],
 ];
